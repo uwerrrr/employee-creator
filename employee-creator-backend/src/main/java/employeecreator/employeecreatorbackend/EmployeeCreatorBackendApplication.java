48756b4770491d7746cb7947378492afeca7ac2e
Oscar (Van NGUYEN) <32070259+uwerrrr@users.noreply.github.com>
2023-08-22 19:29:52 +1000

    initialize backend

---
+++ employee-creator-backend/src/main/java/employeecreator/employeecreatorbackend/EmployeeCreatorBackendApplication.java
@@ -0,0 +1,13 @@
+package employeecreator.employeecreatorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeCreatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeCreatorBackendApplication.class, args);
+	}
+
+}
---
+++ employee-creator-backend/src/test/java/employeecreator/employeecreatorbackend/EmployeeCreatorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package employeecreator.employeecreatorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeCreatorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
