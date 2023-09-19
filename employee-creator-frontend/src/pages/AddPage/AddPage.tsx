3c31ba3b6cf6f59c4c0659dc6ef2d01ef0eb7592
Oscar (Van NGUYEN) <32070259+uwerrrr@users.noreply.github.com>
2023-09-19 15:57:42 +1000

    style add page

--- employee-creator-frontend/src/pages/AddPage/AddPage.tsx
+++ employee-creator-frontend/src/pages/AddPage/AddPage.tsx
@@ -1,13 +1,22 @@
 import EmployeeForm from "../../components/EmployeeForm/EmployeeForm";
 import { Link } from "react-router-dom";
+import style from "./AddPage.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBackward } from "@fortawesome/free-solid-svg-icons";
 
 const AddPage = () => {
   return (
-    <>
-      <Link to={"../"}> {"<"} Back</Link>
-      <h2>Add new employee</h2>
+    <div className={style.page}>
+      <h2 className={style.pageHeading}>
+        <button className={style.backBtn}>
+          <Link className={style.backBtn} to={"../"}>
+            <FontAwesomeIcon icon={faBackward} /> back
+          </Link>
+        </button>
+        Add new employee
+      </h2>
       <EmployeeForm />
-    </>
+    </div>
   );
 };
 
